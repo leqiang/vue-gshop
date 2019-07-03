458f86f333b4d7606f30dbcb19f84fe1cb6783bc
Le Qiang <leqiang1990@163.com>
2019-07-03 22:01:44 +0800

    init gshop

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <router-view/>
+    <FooterGuide v-if="$route.meta.showFooter"/>
+  </div>
+</template>
+
+<script>
+
+import FooterGuide from './components/FooterGuide/FooterGuide'
+
+export default {
+  data(){
+    return {
+
+    }
+  },
+  components: {
+    FooterGuide
+  }
+
+}
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+
+
+</style>
